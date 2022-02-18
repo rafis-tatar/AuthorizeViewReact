15d21e5f96c19353b4faec75dec80a772d3d18bb
hex <rafis-tatar@rambler.ru>
2022-02-18 15:41:52 +0300

    Update loginType.tsx

--- src/Services/login/loginType.tsx
+++ src/Services/login/loginType.tsx
@@ -1,5 +1,5 @@
 export default interface loginType{
-        Login: string;//":"GataullinRA@ec.tatneft.ru",
-        Password : string;//"123edcCDE#@!",
-        IsDomainAccount: Boolean;//: true     
+        Login: string;
+        Password : string;
+        IsDomainAccount: Boolean;
     }
